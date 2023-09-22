multi scaling limits, inhomogenous det array fix

transitfit/output_handler.py
@@ -83,7 +83,8 @@ class OutputHandler:
             # Each entry should be a list containing all the detrending
             # coefficients for the light curve.
             for i in np.ndindex(d.shape):
-                for coeff in np.ravel(global_prior.detrending_coeffs):                   
+                det_coeff_flattened = sum([det for det in global_prior.detrending_coeffs], [])
+                for coeff in det_coeff_flattened: #np.ravel(global_prior.detrending_coeffs):                   
                     if type(coeff) is not list:
                         if self.best_model[coeff][i] is not None:
                             if d[i] is None:
@@ -164,7 +165,8 @@ class OutputHandler:
             # Each entry should be a list containing all the detrending
             # coefficients for the light curve.
             for i in np.ndindex(d.shape):
-                for coeff in np.ravel(global_prior.detrending_coeffs):
+                det_coeff_flattened = sum([det for det in global_prior.detrending_coeffs], [])
+                for coeff in det_coeff_flattened: #np.ravel(global_prior.detrending_coeffs):
                     if type(coeff) is not list:
                         if self.best_model[coeff][i] is not None:
                             if d[i] is None:
@@ -431,8 +433,8 @@ class OutputHandler:
             results_dict = self._initialise_dict_entry(results_dict, param_name)
             if param_name=='norm':
                 self.full_idx_escale=full_idx
-            if param_name=='escale':
-                full_idx=self.full_idx_escale
+            #if param_name=='escale':
+            #    full_idx=self.full_idx_escale
             #print(results_dict,param_name,full_idx)
             if results_dict[param_name][full_idx] is None:
                 # Initialise a list

transitfit/priorinfo.py
@@ -470,14 +470,24 @@ class PriorInfo:
 
         self.normalise = True
 
-    def set_error_scaling(self,lightcurves, scaling_limits):
+    def set_error_scaling(self,lightcurves, scaling_limits, telescope_idx=None):
         
         for i in np.ndindex(lightcurves.shape):
             telescope_idx, filter_idx, epoch_idx = i
 
+            if isinstance(scaling_limits[telescope_idx], Iterable):
+                # The limits for each parameter have been set
+                # individually
+                #low_lim = scaling_limits[telescope_idx][0]
+                #high_lim = scaling_limits[telescope_idx][1]
+                scaling_limits_iter = scaling_limits[telescope_idx]
+
+            else:
+                scaling_limits_iter = scaling_limits
+
             if lightcurves[i] is not None:
-                # A light curve exists. Set up normalisation
-                best, low, high = lightcurves[i].set_error_scaling(scaling_limits)
+                # A light curve exists. Set up error scaling
+                best, low, high = lightcurves[i].set_error_scaling(scaling_limits_iter)
                 self.add_uniform_fit_param('escale', low, high, telescope_idx, filter_idx, epoch_idx)
 
         self.error_scaling = True

transitfit/retriever.py
@@ -9,6 +9,7 @@ import os, csv
 import traceback
 import multiprocessing as mp
 import pandas as pd
+from collections.abc import Iterable
 
 import io
 
@@ -228,10 +229,18 @@ class Retriever:
             if error_scaling_limits is None or len(error_scaling_limits)!=2:
                 self.scaling_limits = [1e-7,10]
             else:
-                error_scaling_limits = np.sort(np.abs(error_scaling_limits))
-                if error_scaling_limits[0]<=0: 
-                    error_scaling_limits[0] = 1e-7
-                self.scaling_limits = error_scaling_limits
+                if isinstance(error_scaling_limits[0], Iterable):
+                    for esl in range(len(error_scaling_limits)):
+                        error_scaling_limits[esl] = np.sort(np.abs(error_scaling_limits[esl]))
+                        if error_scaling_limits[esl][0]<=0: 
+                            error_scaling_limits[esl][0] = 1e-7
+                    self.scaling_limits = error_scaling_limits
+
+                else:
+                    error_scaling_limits = np.sort(np.abs(error_scaling_limits))
+                    if error_scaling_limits[0]<=0: 
+                        error_scaling_limits[0] = 1e-7
+                    self.scaling_limits = error_scaling_limits
 
 
         if detrend: