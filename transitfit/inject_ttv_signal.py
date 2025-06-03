113f83963b3e795ee1e9c9cfb1222b076b23e3d6
Akshay Priyadarshi <akshay.priyadarshi@manchester.ac.uk>
2025-06-03 21:37:37 +0100

    Add logic to find the appropriate index for TTV signal injection in inject_ttv function

--- transitfit/inject_ttv_signal.py
+++ transitfit/inject_ttv_signal.py
@@ -101,3 +101,9 @@ def inject_ttv(times, P, t0, p_prime, p_dprime):
                 period_all=np.append(period_all,P_new)
                 t0_all=np.append(t0_all,t_start+t0_first)
                 indx+=1
+        
+    p_list=[]
+    t0_list=[]
+    for i in range(len(times)):
+        find_id=t0_all-times[i][-1]
+        id=np.argmax(find_id[find_id<=0])
