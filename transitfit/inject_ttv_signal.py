fda01e0855febc8991fc09260903045bbc5ac97c
Akshay Priyadarshi <akshay.priyadarshi@manchester.ac.uk>
2025-06-03 21:32:18 +0100

    Add get_prior_value function to retrieve prior values from DataFrame

--- transitfit/inject_ttv_signal.py
+++ transitfit/inject_ttv_signal.py
@@ -5,3 +5,22 @@ import pandas as pd
 import time
 import os
 from .ttv_fitting import get_shift_in_time_due_to_ttv,get_time_duration, taylor_series
+
+
+def get_prior_value(param, priors_df):
+    """    Get the prior value for a given parameter from the priors DataFrame.
+    Args:
+        param (str): The name of the parameter to retrieve.
+        priors_df (pd.DataFrame): DataFrame containing prior information.
+    Returns:
+        float: The prior value for the specified parameter.
+    """
+    _selected_row = priors_df[priors_df['Parameter'] == param]
+    if _selected_row.empty:
+        raise ValueError(f"Parameter '{param}' not found in priors DataFrame.")
+    if _selected_row['Distribution'].iloc[0].lower() == 'gaussian':
+        value = _selected_row['Input_A'].iloc[0]
+    else:
+        value = np.mean(_selected_row['Input_A'].iloc[0], _selected_row['Input_B'].iloc[0])
+
+    return value
